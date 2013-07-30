ebc425b383fa5675a9422efa0b8b9ba24978dfed
DnMllr <yajnavalkya@cox.net>
2013-07-30 12:47:30 -0700

    Got started in earnest.
    Working on UI. Working functionality includes getting new tweets and
    dates. Currently attempting to impliment a clever way to scroll through
    tweets.

--- data_generator.js
+++ data_generator.js
@@ -44,6 +44,7 @@ var generateRandomTweet = function(){
   tweet.message = randomMessage();
   tweet.created_at = new Date();
   addTweet(tweet);
+  $("#refresh").click()
 };
 
 for(var i = 0; i < 10; i++){
